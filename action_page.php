37d47c219160475282538d7fc143aba9dc35095e
Yash Jadhav <117999797+YashJadhav713@users.noreply.github.com>
2024-10-06 00:27:58 -0700

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Action</title>
+</head>
+<body>
+    
+
+Name: <?php echo $_POST["name"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Subject: <?php echo $_POST["Subject"]; ?><br>
+Msg: <?php echo $_POST["Message"]; ?><br>
+
+</body>
+</html>
